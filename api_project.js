ad9cd18ea9ed3db9af969a26bf3501feb18b5aa4
om <om@oms-MacBook-Pro.local>
2017-10-22 19:43:55 +0530

    .

--- api_project.js
+++ api_project.js
@@ -1,1 +1,1 @@
-define({  "name": "",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2017-10-22T13:43:08.849Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
+define({  "name": "",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2017-10-22T14:13:20.132Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
